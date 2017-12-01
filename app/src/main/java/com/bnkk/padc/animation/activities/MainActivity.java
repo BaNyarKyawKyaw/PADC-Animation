1011a3e34d9cd00dd3feffe9cea250bb50cbe1a7
DESKTOP-RPUASUS\E5-575G <tmmbnkk1999@gmail.com>
2017-12-01 22:51:31 +0630

    Adding Assigment animation!!!

---
+++ app/src/main/java/com/bnkk/padc/animation/activities/MainActivity.java
@@ -0,0 +1,95 @@
+package com.bnkk.padc.animation.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.bnkk.padc.animation.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolBar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.main_screen_coordinatorLayout)
+    CoordinatorLayout mainScreenCoordinatorLayout;
+
+    private Animation openUserProfile;
+    private Animation closeUserProfile;
+    private boolean isUserProfileOpened = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolBar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        openUserProfile = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.open_user_profile);
+        closeUserProfile = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.close_user_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_open_user_profile:
+                isUserProfileOpened = true;
+                mainScreenCoordinatorLayout.startAnimation(openUserProfile);
+                drawerLayout.openDrawer(GravityCompat.END);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (isUserProfileOpened) {
+            isUserProfileOpened = false;
+            mainScreenCoordinatorLayout.startAnimation(closeUserProfile);
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/bnkk/padc/animation/components/UserDataViewPod.java
@@ -0,0 +1,24 @@
+package com.bnkk.padc.animation.components;
+
+import android.content.Context;
+import android.support.design.widget.NavigationView;
+import android.util.AttributeSet;
+
+/**
+ * Created by E5-575G on 12/1/2017.
+ */
+
+public class UserDataViewPod extends NavigationView {
+
+    public UserDataViewPod(Context context) {
+        super(context);
+    }
+
+    public UserDataViewPod(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UserDataViewPod(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
